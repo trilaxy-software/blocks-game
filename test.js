4a9e86b4dfcd4e81dba8140cebdec0900c04757b
Lars Löffler <lars.loeffler@infodesire.com>
2021-08-08 21:10:38 +0200

    rework the game over locig with a red high line

--- test.js
+++ test.js
@@ -5,15 +5,24 @@ test.runAll()
 
 test.showResults( 'output' )
 
-function testRandomPiece() {
+function testRandomPieceFullArrayRange() {
 
-  this.assertEqual( 'all pieces should be able to picked', 
-    pieces.length - 1, Math.floor( 0.9999 * pieces.length ) )
+  this.assertEqual( '(almost) highest random number should produce hightes array index', 
+    pieces.length - 1, Math.floor( 0.999999 * pieces.length ) )
 
-}
+  this.assertEqual( 'lowest random number should produce lowest array index', 
+    0, Math.floor( 0.000001 * pieces.length ) )
+
+  }
 
 function testRandomPiecePosition() {
 
-  this.log( pieces )
+  init()
+  for( let i = 0; i < pieces.length; i++ ) {
+    let piece = pieces[ i ]
+    let copy = piece.duplicate()
+    copy = moveToXPosition( copy, 0 )
+    test.assertEqual( "Piece " + i + " Position 0", 0, copy.x + copy.left )
+  }
 
 }
